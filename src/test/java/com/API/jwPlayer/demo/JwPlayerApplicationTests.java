9fb9f7754d9e4f411f1147453ea198aea51c2cb0
Luc Janssen <lphjanss@gmail.com>
2020-03-16 09:48:41 +0100

    Initialisation  SpringBoot

---
+++ src/test/java/com/API/jwPlayer/demo/JwPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.jwPlayer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
